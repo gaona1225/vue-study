8615694ea6db273cf284e18360e669faf3632975
gaona02 <gaona02@baidu.com>
2018-03-26 16:19:34 +0800

    vue-cli scss

--- vue-study-demo/src/pagination/pagination.vue
+++ vue-study-demo/src/pagination/pagination.vue
@@ -25,14 +25,14 @@ export default {
 }
 </script>
 
-<style type="text/css">
+<style lang = "less">
 @import url('https://fonts.googleapis.com/css?family=Lato|Roboto:700');
 *:focus {
     outline: none;
 }
 body {
     background-color: #777;
-    background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAQAAABuBnYAAAAAU0lEQVQIHQXBwRGDIBAAwO2/AMcCDHAcPjIRxjJ8Je1kl1uqUgphsWu6w0sIG6npLpcUBql4e/wsVRKabrkNTacIYbMrwsF06rqUhsnXVKVT+Hj+Ue4rPSONk4kAAAAASUVORK5CYII=);
+    background-image:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAQAAABuBnYAAAAAU0lEQVQIHQXBwRGDIBAAwO2/AMcCDHAcPjIRxjJ8Je1kl1uqUgphsWu6w0sIG6npLpcUBql4e/wsVRKabrkNTacIYbMrwsF06rqUhsnXVKVT+Hj+Ue4rPSONk4kAAAAASUVORK5CYII=');
     width: 100vw;
     height: 100vh;
     display: flex;
@@ -56,37 +56,35 @@ body {
     display: flex;
     justify-content: center;
     align-items: center;
-}
-
-.pagination li {
-    margin: 0 4px;
-    padding: 0;
-    list-style: none outside none;
-    line-height: 25px;
-}
-
-.pagination button {
-    display: block;
-    border: none 0;
-    color: #f2f2f2;
-    text-shadow: 1px 0 0 #101011;
-    padding: .5em 1.5em;
-    border-radius: 2px;
-    box-shadow: 0 1px 0 #5a5b5c inset, 0 1px 0 #080808;
-    background: linear-gradient(to top, #434345, #2f3032);
-    text-decoration: none;
-    cursor: pointer;
-    font-family: "Microsoft Yahei", sans-serif;
-    font-size: 100%;
-}
-.pagination button:hover {
-    text-decoration: none;
-    box-shadow: 0 1px 0 #f9bd71 inset, 0 1px 0 #0a0a0a;
-    background: linear-gradient(to top, #c87100, #f48b03);
-}
-.pagination button.active {
-    text-decoration: none;
-    box-shadow: 0 1px 0 #f9bd71 inset, 0 1px 0 #0a0a0a;
-    background: linear-gradient(to top, #f48b03, #c87100);
+    li {
+        margin: 0 4px;
+        padding: 0;
+        list-style: none outside none;
+        line-height: 25px;
+    }
+    button {
+        display: block;
+        border: none 0;
+        color: #f2f2f2;
+        text-shadow: 1px 0 0 #101011;
+        padding: .5em 1.5em;
+        border-radius: 2px;
+        box-shadow: 0 1px 0 #5a5b5c inset, 0 1px 0 #080808;
+        background: linear-gradient(to top, #434345, #2f3032);
+        text-decoration: none;
+        cursor: pointer;
+        font-family: "Microsoft Yahei", sans-serif;
+        font-size: 100%;
+        &:hover {
+            text-decoration: none;
+            box-shadow: 0 1px 0 #f9bd71 inset, 0 1px 0 #0a0a0a;
+            background: linear-gradient(to top, #c87100, #f48b03);
+        }
+        &.active {
+            text-decoration: none;
+            box-shadow: 0 1px 0 #f9bd71 inset, 0 1px 0 #0a0a0a;
+            background: linear-gradient(to top, #f48b03, #c87100);
+        }
+    }
 }
 </style>
